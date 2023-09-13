f62ed3e1c31e1247aa078635da87c53047dc19d6
TingFeng Shen <tingfengshen@TingFengdeMacBook-Air.local>
2023-09-13 16:56:58 +0800

    first commit

---
+++ LiFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiFrame
+//
+//  Created by TingFeng Shen on 2023/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
