e674699d6b95a09af72ae1a993e7d6762111408b
TingFeng Shen <tingfengshen@TingFengdeMacBook-Air.local>
2023-09-15 18:11:11 +0800

    change camera interface by self made, shutter cancel & flash

--- LiFrame/Camera/CameraViewController.swift
+++ LiFrame/Camera/CameraViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 import PhotosUI
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+    let imageCameraPicker = UIImagePickerController()
+    var layerImageView: UIImageView?
+    let flashButton: UIButton = {
+        let button = UIButton()
+        button.isSelected = false
+        button.setImage(UIImage(systemName: "bolt.slash.fill"), for: .normal)
+        button.setImage(UIImage(systemName: "bolt.fill"), for: .selected)
+        button.frame = CGRect(x: 100, y: 600, width: 50, height: 50)
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        flashButton.addTarget(self, action: #selector(tappedFlash), for: .touchUpInside)
     }
     @IBAction func choosePhotoToCamera(_ sender: Any) {
         var configuration = PHPickerConfiguration()
@@ -26,26 +35,28 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
 
 extension CameraViewController: PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-//        picker.dismiss(animated: true)
         let itemProviders = results.map(\.itemProvider)
         if let itemProvider = itemProviders.first, itemProvider.canLoadObject(ofClass: UIImage.self) {
-            itemProvider.loadObject(ofClass: UIImage.self) {[weak self] (image, error) in
+            itemProvider.loadObject(ofClass: UIImage.self) { [weak self] (image, error) in
                 DispatchQueue.main.async {
                     guard let self = self, let image = image as? UIImage else { return }
                     // 設定相機
-                    let imageCameraPicker = UIImagePickerController()
-                    imageCameraPicker.sourceType = .camera
-                    imageCameraPicker.delegate = self
+                    self.imageCameraPicker.sourceType = .camera
+                    self.setButtonsUI()
+                    self.imageCameraPicker.delegate = self
+                    self.imageCameraPicker.showsCameraControls = false
                     if UIImagePickerController.isSourceTypeAvailable(.camera) {
-                        let smallImage = self.resizeImage(image: image, width: 320)
-                        let imageView =  UIImageView(image: smallImage)
-                        imageView.frame = CGRect(x: 0, y: 123, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width*4/3)
-                        imageView.alpha = 0.3
-                        //開啟相機
-                        imageCameraPicker.cameraOverlayView?.addSubview(imageView)
-                        imageCameraPicker.cameraOverlayView?.translatesAutoresizingMaskIntoConstraints = false
-                        imageCameraPicker.cameraOverlayView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive
-                        self.show(imageCameraPicker, sender: self)
+//                        let smallImage = self.resizeImage(image: image, width: 320)
+                        let imageView =  UIImageView(image: image)
+                        let screenSize = UIScreen.main.bounds
+                        imageView.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.width/3*4)
+                        self.layerImageView = imageView
+                        guard let layerImageView = self.layerImageView else { return }
+                        layerImageView.alpha = 0.3
+                        // 開啟相機
+                        self.imageCameraPicker.cameraOverlayView?.addSubview(layerImageView)
+                        self.imageCameraPicker.cameraOverlayView?.translatesAutoresizingMaskIntoConstraints = false
+                        self.show(self.imageCameraPicker, sender: self)
                     }
                 }
             }
@@ -55,16 +66,58 @@ extension CameraViewController: PHPickerViewControllerDelegate {
         guard let image = info[.originalImage] as? UIImage else { return }
 
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-        dismiss(animated: true, completion: nil)
     }
-    // 重新調整大小
-    func resizeImage(image: UIImage, width: CGFloat) -> UIImage {
-            let size = CGSize(width: width, height:
-                image.size.height * width / image.size.width)
-            let renderer = UIGraphicsImageRenderer(size: size)
-            let newImage = renderer.image { (context) in
-                image.draw(in: renderer.format.bounds)
-            }
-            return newImage
+//    // 重新調整大小
+//    func resizeImage(image: UIImage, width: CGFloat) -> UIImage {
+//            let size = CGSize(width: width, height:
+//                image.size.height * width / image.size.width)
+//            let renderer = UIGraphicsImageRenderer(size: size)
+//            let newImage = renderer.image { (context) in
+//                image.draw(in: renderer.format.bounds)
+//            }
+//            return newImage
+//    }
+    func setButtonsUI() {
+        let shutter: UIButton = {
+            let button = UIButton()
+            button.setImage(UIImage(systemName: "camera.circle"), for: .normal)
+            button.addTarget(self, action: #selector(tappedShutter), for: .touchUpInside)
+            button.frame = CGRect(x: 200, y: 600, width: 50, height: 50)
+            button.tintColor = .white
+            let configuration = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 200))
+            button.setPreferredSymbolConfiguration(configuration, forImageIn: .normal)
+            return button
+        }()
+        let cancel: UIButton = {
+            let button = UIButton()
+            button.setTitle("Cancel", for: .normal)
+            button.addTarget(self, action: #selector(tappedCancel), for: .touchUpInside)
+            button.frame = CGRect(x: 200, y: 650, width: 50, height: 50)
+            button.tintColor = .white
+//            let configuration = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 200))
+//            button.setPreferredSymbolConfiguration(configuration, forImageIn: .normal)
+            return button
+        }()
+        imageCameraPicker.cameraFlashMode = .off
+        flashButton.isSelected = false
+        imageCameraPicker.view.addSubview(shutter)
+        imageCameraPicker.view.addSubview(flashButton)
+        imageCameraPicker.view.addSubview(cancel)
+    }
+    @objc func tappedShutter() {
+        imageCameraPicker.cameraDevice = .rear
+        imageCameraPicker.takePicture()
+        layerImageView?.removeFromSuperview()
+    }
+    @objc func tappedFlash() {
+        flashButton.isSelected.toggle()
+        if imageCameraPicker.cameraFlashMode == .off {
+            imageCameraPicker.cameraFlashMode = .on
+        } else {
+            imageCameraPicker.cameraFlashMode = .off
+        }
+    }
+    @objc func tappedCancel() {
+        dismiss(animated: true)
     }
 }
